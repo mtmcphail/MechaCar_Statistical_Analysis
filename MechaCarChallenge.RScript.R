d0899798f00af029e0454a71585aa49f0291d1f5
MARGARET T MCPHAIL <margarettmcphail@MARGARETs-MacBook-Pro.local>
2021-01-18 16:06:15 -0800

    Upload Challenge 15 files.

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -37,21 +37,27 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PS
                                           Std_Dev_PSI=sd(PSI),
                                           Num_Coil=n(), .groups = 'keep')                                      
 
+#box plot: PSI Whole lot
+plt1 <- ggplot(mecha_coil,aes(y=PSI)) #import dataset into ggplot2
+plt1 + geom_boxplot() #add boxplot
 
+#box plot: PSI each indicdiual Lot
+plt2 <- ggplot(mecha_coil,aes(x=Manufacturing_Lot,y=PSI)) #import dataset into ggplot2
+plt2 + geom_boxplot()
 
 ### CHALLENGE 15: DELIVERABLE 3
 
 #1. use t.test() to determine if the PSI across ALL lots is statistically different from the pop. mean of 1,500 PSI.
-t.test((mecha_coil$PSI),mu=mean((mecha_coil$PSI)))
+t.test(mecha_coil$PSI,mu=1500)
 
 
 #2. Use t.test() function 3 more times with subset() to determine if PSI for each manufacturing lot is statistically different from the pop. mean of 1,500 PSI
-t.test((subset(mecha_coil,manufacturing_lot='lot1')$PSI),mu=mean(mecha_coil$PSI))
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
 
-t.test((subset(mecha_coil,manufacturing_lot='lot2')$PSI),mu=mean(mecha_coil$PSI))
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
 
-t.test((subset(mecha_coil,manufacturing_lot='lot3')$PSI),mu=mean(mecha_coil$PSI))
-
-  
-
-
+# log10 was not used as the data was not skewed and did not need any smoothing
